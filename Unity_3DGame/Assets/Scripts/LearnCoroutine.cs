1e5793133d17063bd923e72400cfb7df601d8e1c
Bing866 <114146062+Bing866@users.noreply.github.com>
2023-01-08 03:27:46 +0800

    up date 13 week

--- Unity_3DGame/Assets/Scripts/LearnCoroutine.cs
+++ Unity_3DGame/Assets/Scripts/LearnCoroutine.cs
@@ -15,9 +15,18 @@ namespace BING
         // 3. 方法內必須使用 yield return (等待)
         // 4. 使用 StartCoroutine 啟動
 
+        // 字串 string 為 char 陣列
+        private string testDialogue = "這裡好恐怖，我想要快點離開...";
+
         private void Awake()
         {
-            StartCoroutine(Test());
+            // StartCoroutine(Test());
+
+            // print("取得測試對話的第一個字:" + tesDialogue[0]);
+
+            // StartCoroutine(ShowDialgue());
+
+            StartCoroutine(ShowDialogueUseFor());
         }
 
         private IEnumerator Test()
@@ -27,6 +36,25 @@ namespace BING
             print("<color=#ff3333>第二行程式</color>");
             yield return new WaitForSeconds(3);
             print("<color=#3333ff>第三行程式</color>");
+        }
+
+        private IEnumerator ShowDialogue() 
+        {
+            print(testDialogue[0]);
+            yield return new WaitForSeconds(0.1f);
+            print(testDialogue[1]);
+            yield return new WaitForSeconds(0.1f);
+            print(testDialogue[2]);
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        private IEnumerator ShowDialogueUseFor() 
+        {
+            for (int i = 0; i < testDialogue.Length; i++) 
+            {
+                print(testDialogue[i]);
+                yield return new WaitForSeconds(0.2f);
+            }
         }
     }
 }
